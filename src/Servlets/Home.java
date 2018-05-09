2126793d7127d9affdb0a8205d850da93797aa94
gpequery <gregory.pequery.sio@gmail.com>
2018-05-09 20:56:55 +0200

    init first view with servlet

---
+++ src/Servlets/Home.java
@@ -0,0 +1,14 @@
+package Servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Home extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        request.getRequestDispatcher("Views/home.jsp").forward(request, response);
+    }
+}
